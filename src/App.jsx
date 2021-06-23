084e229dc905058344e250404585afa704d1a89f
Irina <ioblomova13@gmail.com>
2021-06-23 15:21:21 +0300

    add introduction page

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import Testimonials from './components/testimonials/Testimonials'
 import Topbar from './components/topbar/Topbar'
 import Works from './components/works/Works'
 import './app.scss'
+import { useState } from 'react';
+import Menu from './components/menu/Menu'
 
 
 const App = () => {
+    const [menuOpen, setMenuOpen] = useState(true)
     return (
         <div className="app">
-            <Topbar />
+            <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
             <div className="sections">
-                
+
                 <Intro />
                 <Portfolio />
                 <Works />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,9 +2,9 @@ import React from "react"
 import "./topbar.scss"
 import {Person, Mail} from "@material-ui/icons"
 
-const Topbar = () => {
+const Topbar = ({menuOpen, setMenuOpen}) => {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">genius.</a>
@@ -18,7 +18,7 @@ const Topbar = () => {
                     </div>
                 </div>
                 <div className="right">
-                    <div className="hamburger">
+                    <div className="hamburger" onClick={ () => setMenuOpen (!menuOpen) }>
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
