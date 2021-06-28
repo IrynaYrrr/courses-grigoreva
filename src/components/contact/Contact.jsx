43b87822cb7b7a7cca152ef74d757a49b2511d60
Irina <ioblomova13@gmail.com>
2021-06-28 14:40:02 +0300

    add include mobile

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,17 +3,16 @@ import "./contact.scss"
 import { useState } from "react"
 
 const Contact = () => {
-
-    const [message, setMessage] = useState(false)
+    const [message, setMessage] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setMessage(true)
-    }
+        setMessage(true);
+    };
     return (
         <div className="contact" id="contact">
             <div className="left">
-                <img src="assets/snake.svg" alt="" />
+                <img src="assets/shake.svg" alt="" />
             </div>
             <div className="right">
                 <h2>Contact.</h2>
@@ -21,11 +20,11 @@ const Contact = () => {
                     <input type="text" placeholder="Email" />
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">Send</button>
-                    {message && <span>Thanks, I will reply ASAP :)</span>}
+                    {message && <span>Thanks, I'll reply ASAP :)</span>}
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
 export default Contact
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { useState } from "react"
-import "./works.scss"
+import React from "react"
+import { useState } from "react";
+import "./works.scss";
+
 
 const Works = () => {
-    const [currentSlide,setCurrentSlide] = useState(0);
-
+    const [currentSlide, setCurrentSlide] = useState(0);
     const data = [
         {
             id: "1",
@@ -35,14 +35,17 @@ const Works = () => {
         },
     ];
 
-    const handleClick = (way)=> {
-        way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) : setCurrentSlide(currentSlide < data.lenght-1 ? currentSlide+1 : 0);
+    const handleClick = (way) => {
+        way === "left"
+            ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+            : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
     };
 
     return (
         <div className="works" id="works">
-            <div className="slider"
-            style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+            <div
+                className="slider"
+                style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
             >
                 {data.map((d) => (
                     <div className="container">
@@ -53,37 +56,34 @@ const Works = () => {
                                         <img src={d.icon} alt="" />
                                     </div>
                                     <h2>{d.title}</h2>
-                                    <p>
-                                        {d.desc}
-                                    </p>
+                                    <p>{d.desc}</p>
                                     <span>Projects</span>
                                 </div>
                             </div>
                             <div className="right">
-                                <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                                <img
+                                    src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
                                     alt=""
                                 />
                             </div>
                         </div>
-
                     </div>
                 ))}
-
             </div>
-            
-            <img src="assets/arrow.png"
-            className="arrow left"
-            alt=""
-            onClick={ () => handleClick("left") } />
-
-            <img src="assets/arrow.png"
-             className="arrow right"
-             alt=""
-             onClick={ () => handleClick("") } />
-
+            <img
+                src="assets/arrow.png"
+                className="arrow left"
+                alt=""
+                onClick={() => handleClick("left")}
+            />
+            <img
+                src="assets/arrow.png"
+                className="arrow right"
+                alt=""
+                onClick={() => handleClick()}
+            />
         </div>
-    )
-
+    );
 }
 
-export default Works
+export default Works;
