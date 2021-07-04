3e3ff5ef73b68ae6b8c9f184c080f681f146caa4
Irina <ioblomova13@gmail.com>
2021-07-04 22:34:59 +0300

    add test contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./contact.scss"
 import { useState } from "react"
+import { Typography, Card, CardContent, Grid, TextField, Button } from "@material-ui/core"
 
 const Contact = () => {
     const [message, setMessage] = useState(false);
@@ -14,14 +15,69 @@ const Contact = () => {
             <div className="left">
                 <img src="assets/shake.svg" alt="" />
             </div>
+
             <div className="right">
-                <h2>Contact.</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Email" />
-                    <textarea placeholder="Message"></textarea>
-                    <button type="submit">Send</button>
-                    {message && <span>Thanks, I'll reply ASAP :)</span>}
-                </form>
+                <Typography class="typography">
+                    Запись на курс
+                </Typography>
+                <Card class="card">
+                    <CardContent>
+                        <form class="form">
+                            <Grid container spacing={1}>
+
+                                <Grid xs={12} sm={6} item>
+                                    <TextField
+                                        class="item"
+                                        label="Выберите курс"
+                                        placeholder="Выберите курс"
+                                        variant="outlined"
+                                        fullWidth
+                                        required />
+                                </Grid>
+                                <Grid xs={12} sm={6} item>
+                                    <TextField
+                                        class="item"
+                                        label="Фамилия, имя"
+                                        placeholder="Фамилия, имя"
+                                        variant="outlined"
+                                        fullWidth
+                                        required />
+                                </Grid>
+                                <Grid xs={12} sm={6} item>
+                                    <TextField
+                                        class="item"
+                                        label="Номер телефона"
+                                        type="number"
+                                        placeholder="Введите номер телефона"
+                                        variant="outlined"
+                                        fullWidth
+                                        required />
+                                </Grid>
+                                <Grid xs={12} sm={6} item>
+                                    <TextField
+                                        class="item"
+                                        label="Введите сообщение"
+                                        multiline rows={5}
+                                        placeholder="Введите сообщение"
+                                        variant="outlined"
+                                        fullWidth
+                                        required />
+                                </Grid>
+                                <Grid xs={12} sm={6} item>
+                                    <Button
+                                        class="button"
+                                        type="submit"
+                                        variant="contained"
+                                        color="primary"
+                                        fullWidth>
+                                        Отправить
+                                    </Button>
+                                </Grid>
+
+                            </Grid>
+                        </form>
+                    </CardContent>
+                </Card>
             </div>
         </div>
     );
