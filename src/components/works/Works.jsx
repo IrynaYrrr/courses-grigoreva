7dcbc7c5672df325728d97e01c7a35cade88c52d
Irina <ioblomova13@gmail.com>
2021-07-24 00:37:22 +0300

    add Work change

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -8,30 +8,13 @@ const Works = () => {
     const data = [
         {
             id: "1",
-            icon: "./assets/mobile.png",
-            title: "Web Design",
-            desc:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-            img:
-                "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+            imgSem: "./assets/VSseminarBuh.png",
+            imgProg: "./assets/VSprogramBuh.png",
         },
         {
             id: "2",
-            icon: "./assets/globe.png",
-            title: "Mobile Application",
-            desc:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-            img:
-                "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
-        },
-        {
-            id: "3",
-            icon: "./assets/writing.png",
-            title: "Branding",
-            desc:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-            img:
-                "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+            imgSem: "./assets/VSseminarWork.png",
+            imgProg: "./assets/VSprogramWork.png",
         },
     ];
 
@@ -51,20 +34,10 @@ const Works = () => {
                     <div className="container">
                         <div className="item">
                             <div className="left">
-                                <div className="leftContainer">
-                                    <div className="imgContainer">
-                                        <img src={d.icon} alt="" />
-                                    </div>
-                                    <h2>{d.title}</h2>
-                                    <p>{d.desc}</p>
-                                    <span>Projects</span>
-                                </div>
+                                <img src={d.imgSem} alt="" />
                             </div>
                             <div className="right">
-                                <img
-                                    src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
-                                    alt=""
-                                />
+                                <img src={d.imgProg} alt="" />
                             </div>
                         </div>
                     </div>
@@ -74,7 +47,7 @@ const Works = () => {
                 src="assets/arrow.png"
                 className="arrow left"
                 alt=""
-                onClick={() => handleClick("left")}
+                onClick={() => handleClick()}
             />
             <img
                 src="assets/arrow.png"
