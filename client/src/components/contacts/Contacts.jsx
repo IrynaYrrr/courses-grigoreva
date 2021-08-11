1fc346d6dfc9b55e00bc4932afe169edd0c3b547
Irina <ioblomova13@gmail.com>
2021-08-11 17:45:26 +0300

    update changes

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -1,17 +1,3 @@
-import { useState } from 'react';
-import { makeStyles } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import MuiPhoneInput from 'material-ui-phone-number';
 import "./contacts.scss";
 
 const Contacts = () => {
