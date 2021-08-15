d1c23aff8e608f2f843a6bfe0fa23183cded8f86
Irina <ioblomova13@gmail.com>
2021-08-15 19:29:31 +0300

    create Certificate components

---
+++ client/src/components/certificatee/Certificate.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./certificate.scss"
+
+const Certificate = () => {
+
+    return (
+        <div className="certificate" id="certificate">
+            <h1>Информация о сертификате</h1>
+        </div>
+    )
+}
+
+export default Certificate;
