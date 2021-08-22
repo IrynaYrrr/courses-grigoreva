48775e26269f88a2851f15133c768cbf13fc8af5
Irina <ioblomova13@gmail.com>
2021-08-22 15:49:14 +0300

    add comments

---
+++ client/src/components/comments/Comments.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./comments.scss"
+
+const Comments = () => {
+
+    return (
+        <div className="comments" id="comments">
+            <h1>Информация о комментариях</h1>
+        </div>
+    )
+}
+
+export default Comments;
