9a7371d3cc5caa78ff5c5ea1d45a38833eee5723
Irina <ioblomova13@gmail.com>
2021-09-20 16:06:41 +0300

    apply styles for mobile reviews

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -1,49 +1,108 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { nanoid } from 'nanoid';
 import './Reviews.scss';
 
-const data = [
+let data = [
+        {
+            imgleft: './assets/chat2.jpg',
+            imgcenter: './assets/chat1.jpg',
+            imgright: './assets/chat3.jpg'
+        },
+        {
+            imgleft: './assets/chat4.jpg',
+            imgcenter: './assets/chat6.jpg',
+            imgright: './assets/chat5.jpg'
+        },
+        {
+            imgleft: './assets/chat7.jpg',
+            imgcenter: './assets/chat8.jpg',
+            imgright: './assets/chat9.jpg'
+        },
+        {
+            imgleft: './assets/chat10.jpg',
+            imgcenter: './assets/chat11.jpg',
+            imgright: './assets/chat12.jpg'
+        },
+    ]
+;
+
+for (let i = 0; i < 3; i++) {
+    data.push(...data);
+}
+
+data = data.map((d) => {
+    const nd = { ...d };
+    nd.id = nanoid();
+    return nd;
+});
+
+let data2 = [
     {
-        id: 1000000,
-        imgleft: './assets/chat2.jpg',
-        imgcenter: './assets/chat1.jpg',
-        imgright: './assets/chat3.jpg'
+        img: './assets/chat2.jpg',
     },
     {
-        id: 1000001,
-        imgleft: './assets/chat4.jpg',
-        imgcenter: './assets/chat6.jpg',
-        imgright: './assets/chat5.jpg'
+        img: './assets/chat1.jpg',
     },
     {
-        id: 1000002,
-        imgleft: './assets/chat7.jpg',
-        imgcenter: './assets/chat8.jpg',
-        imgright: './assets/chat9.jpg'
+        img: './assets/chat3.jpg'
     },
     {
-        id: 1000003,
-        imgleft: './assets/chat10.jpg',
-        imgcenter: './assets/chat11.jpg',
-        imgright: './assets/chat12.jpg'
+        img: './assets/chat4.jpg',
+    },
+    {
+        img: './assets/chat6.jpg',
+    },
+    {
+        img: './assets/chat5.jpg'
+    },
+    {
+        img: './assets/chat7.jpg',
+    },
+    {
+        img: './assets/chat8.jpg',
+    },
+    {
+        img: './assets/chat9.jpg'
+    },
+    {
+        img: './assets/chat10.jpg',
+    },
+    {
+        img: './assets/chat11.jpg',
+    },
+    {
+        img: './assets/chat12.jpg'
     },
 ];
 
-for (let i = 0; i < 4; i++) {
-    data.push(...data);
+for (let i = 0; i < 2; i++) {
+    data2.push(...data2);
 }
+
+data2 = data2.map((d) => {
+    const nd = { ...d };
+    nd.id = nanoid();
+    return nd;
+});
+
 const Reviews = () => {
-    const [currentSlide, setCurrentSlide] = useState(data.length / 2);
+    const isMobile = useMediaQuery({ query: `(max-width: 768px)` });
+
+    const currentData = isMobile ? data2 : data;
+
+    const [currentSlide, setCurrentSlide] = useState(currentData.length / 2);
 
     const handleClick = (way) => {
         if (way === 'left') {
             if (currentSlide === 0) {
-                setCurrentSlide(data.length - 1);
+                setCurrentSlide(currentData.length - 1);
             } else {
                 setCurrentSlide(currentSlide - 1);
             }
         } else {
-            if (currentSlide === data.length - 1) {
+            if (currentSlide === currentData.length - 1) {
                 setCurrentSlide(0);
             } else {
                 setCurrentSlide(currentSlide + 1);
@@ -57,25 +116,36 @@ const Reviews = () => {
                 className="slider"
                 style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
             >
-                {data.map((d) => (
-                    <div className="container">
-                        <div className="item">
-                            <div className="left">
-                                <img src={d.imgleft} alt="" />
+                {isMobile ?
+                    currentData.map((d) => (
+                        <div key={d.id} className="container">
+                            <div className="item">
+                                <div className="center">
+                                    <img src={d.img} alt="" />
+                                </div>
                             </div>
                         </div>
-                        <div className="item">
-                            <div className="center">
-                                <img src={d.imgcenter} alt="" />
+                    )) :
+                    currentData.map((d) => (
+                        <div key={d.id} className="container">
+                            <div className="item">
+                                <div className="left">
+                                    <img src={d.imgleft} alt="" />
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="center">
+                                    <img src={d.imgcenter} alt="" />
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="right">
+                                    <img src={d.imgright} alt="" />
+                                </div>
                             </div>
                         </div>
-                        <div className="item">
-                            <div className="right">
-                                <img src={d.imgright} alt="" />
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                    ))
+                }
             </div>
             <img
                 src="assets/arrow.png"
