08d2d530b5b76b17d223853e38e6386f922e4dd1
Irina <ioblomova13@gmail.com>
2021-09-22 12:56:15 +0300

    fix contacts

--- client/src/components/contacts/Contacts.jsx
+++ client/src/components/contacts/Contacts.jsx
@@ -22,7 +22,7 @@ const useStyles = makeStyles({
         },
         '@media (max-width: 768px)': {
             height: 50,
-            width: 70
+            width: 50
         },
     },
     instagram: {
@@ -36,7 +36,7 @@ const useStyles = makeStyles({
         },
         '@media (max-width: 768px)': {
             height: 50,
-            width: 70
+            width: 50
         },
     },
     linkedin: {
@@ -50,7 +50,7 @@ const useStyles = makeStyles({
         },
         '@media (max-width: 768px)': {
             height: 50,
-            width: 70
+            width: 50
         },
     },
     whatsapp: {
@@ -64,7 +64,7 @@ const useStyles = makeStyles({
         },
         '@media (max-width: 768px)': {
             height: 50,
-            width: 70
+            width: 50
         },
     },
     viber: {
@@ -78,7 +78,7 @@ const useStyles = makeStyles({
         },
         '@media (max-width: 768px)': {
             height: 50,
-            width: 70
+            width: 50
         },
     },
     telegram: {
@@ -92,7 +92,7 @@ const useStyles = makeStyles({
         },
         '@media (max-width: 768px)': {
             height: 50,
-            width: 70
+            width: 50
         },
     },
     gmail: {
@@ -106,7 +106,7 @@ const useStyles = makeStyles({
         },
         '@media (max-width: 768px)': {
             height: 50,
-            width: 70
+            width: 50
         },
     },
 });
@@ -123,7 +123,7 @@ const Contacts = () => {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2645.620887014509!2d35.0479754516061!3d48.4638031362597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbe3e6b1cf0641%3A0x2e649084b63cfed6!2z0KbQtdC90YLRgCDQvtCx0YPRh9C10L3QuNGPICLQk9Cg0JjQk9Ce0KDQrNCV0JLQkCI!5e0!3m2!1sru!2sua!4v1628706056715!5m2!1sru!2sua"
                     width="100%"
                     height="100%"
-                    style={{ border: 0, marginTop: 50, }}
+                    style={{ border: 0, marginTop: 0, }}
                     allowFullScreen=""
                     loading="lazy" />
             </div>
