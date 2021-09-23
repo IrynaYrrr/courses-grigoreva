078e3a2bf7dc8202e4fbb0c06c68a84818907fcb
Irina <ioblomova13@gmail.com>
2021-09-23 13:01:03 +0300

    refactoring Certificate

--- client/src/components/certificatee/Certificate.jsx
+++ client/src/components/certificatee/Certificate.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './certificate.scss'
 import CheckIcon from '@material-ui/icons/Check';
 import Filter1Icon from '@material-ui/icons/Filter1';
 import Filter2Icon from '@material-ui/icons/Filter2';
@@ -8,6 +7,7 @@ import Filter4Icon from '@material-ui/icons/Filter4';
 import Filter5Icon from '@material-ui/icons/Filter5';
 import Filter6Icon from '@material-ui/icons/Filter6';
 import { useMediaQuery } from 'react-responsive';
+import './certificate.scss'
 
 const Certificate = () => {
 
@@ -18,46 +18,39 @@ const Certificate = () => {
             <div className="left">
 
                 {isMobile ?
-
-                    <div className="screen">
-                        <img
-                            className="screenimg"
-                            src="assets/sixsteps.png"
-                            alt="Вы в 6 шагах от цели:
+                    <img className="mobile"
+                         src="assets/sixsteps.png"
+                         alt="Вы в 6 шагах от цели:
                             1.Записываетесь на курс,
                             2.Посещаете занятия,
                             3.Сдаёте итоговый экзамен,
                             4.Получаете сертификат,
                             5.Добавляете навыки в резюме,
                             6.Практикуете знания на работе"
-                        />
-                    </div>
+                    />
+
                     :
-                    <span className="container">
-                      <span className="items">
-                        <h1 className="heading">Вы в 6 шагах от цели:
+
+                    <span className="desktop">
+                        <h1 className="title">Вы в 6 шагах от цели:
                         </h1>
-                        <ul className="ul">
-                            <Filter1Icon /> <li className="li"><CheckIcon /> Записываетесь на курс</li>
-                            <Filter2Icon /> <li className="li"><CheckIcon /> Посещаете занятия</li>
-                            <Filter3Icon /> <li className="li"><CheckIcon /> Сдаёте итоговый экзамен</li>
-                            <Filter4Icon /> <li className="li"><CheckIcon /> Получаете сертификат</li>
-                            <Filter5Icon /> <li className="li"><CheckIcon /> Добавляете навыки в резюме</li>
-                            <Filter6Icon /> <li className="li"><CheckIcon /> Практикуете знания на работе</li>
+                        <ul>
+                            <Filter1Icon /> <li><CheckIcon /> Записываетесь на курс</li>
+                            <Filter2Icon /> <li><CheckIcon /> Посещаете занятия</li>
+                            <Filter3Icon /> <li><CheckIcon /> Сдаёте итоговый экзамен</li>
+                            <Filter4Icon /> <li><CheckIcon /> Получаете сертификат</li>
+                            <Filter5Icon /> <li><CheckIcon /> Добавляете навыки в резюме</li>
+                            <Filter6Icon /> <li><CheckIcon /> Практикуете знания на работе</li>
                         </ul>
-                      </span>
                     </span>
                 }
             </div>
 
             <div className="right">
-                <div className="imgcontainer">
-                    <img
-                        className="imgitem"
-                        src="assets/certificate.png"
-                        alt="certificate"
-                    />
-                </div>
+                <img className="diploma"
+                     src="assets/certificate.png"
+                     alt="Фотокарточка сертификата"
+                />
             </div>
         </div>
     )
