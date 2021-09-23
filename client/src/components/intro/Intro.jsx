68b736372a69b8bf66258fb6399962756fa8c38f
Irina <ioblomova13@gmail.com>
2021-09-23 14:32:34 +0300

    refactoring Intro

--- client/src/components/intro/Intro.jsx
+++ client/src/components/intro/Intro.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import './intro.scss'
 import { init } from 'ityped'
 import { useEffect, useRef } from 'react'
+import './intro.scss'
 
 const Intro = () => {
 
@@ -12,25 +12,32 @@ const Intro = () => {
             showCursor: true,
             backDelay: 1500,
             backSpeed: 60,
-            strings: ['считать налоги', 'вести ФОП', 'сдавать отчётность', 'проходить собеседования','расставлять приоритеты'],
+            strings: [
+                'считать налоги',
+                'вести ФОП',
+                'сдавать отчётность',
+                'проходить собеседования',
+                'расставлять приоритеты'
+            ],
         });
     }, [])
     return (
         <div className="intro" id="intro">
-            <div className="left">
-                <div className="imgContainer">
-                    <img src="assets/mainphoto.jpg" alt="" />
+            <div className="container">
+                <div className="photo">
+                    <img
+                        src="assets/teacher.jpg"
+                        alt="Фото преподавателя"
+                    />
                 </div>
             </div>
-            <div className="right">
-                <div className="wrapper">
+
+            <div className="greeting">
+                <div className="appeal">
                     <h2>Людмила Петровна</h2>
                     <h4>Преподаватель «Центра обучения Григорьева»</h4>
-                    <br/>
-                    <h3> Научу Вас:
-                    <br/>
-                    <br/>
-                    <span ref={textRef}></span></h3>
+                    <br />
+                    <h3>Научу Вас:<br /><br /><span ref={textRef}></span></h3>
                 </div>
             </div>
         </div>
